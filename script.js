7e84fd72bc152154be4777751a29c3b3b61aef9b
Shruti Garg <138093537+Gargshruti19@users.noreply.github.com>
2023-10-31 13:01:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+	voices = window.speechSynthesis.getVoices();
+	speech.voice = voices[0];
+	voices.forEach(
+		(voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+	);
+};
+voiceSelect.addEventListener("change", function () {
+	speech.voice = voices[voiceSelect.value];
+});
+document.querySelector("button").addEventListener("click", function () {
+	speech.text = document.querySelector("textarea").value;
+	window.speechSynthesis.speak(speech);
+});
